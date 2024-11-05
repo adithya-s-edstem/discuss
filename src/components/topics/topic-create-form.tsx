445630442f516354cf6f9399cb727fe48efe9d89
Adithya S Sekhar <adithya.s@edstem.com>
2024-11-05 10:52:22 +0000

    86

--- src/components/topics/topic-create-form.tsx
+++ src/components/topics/topic-create-form.tsx
@@ -12,7 +12,9 @@ import * as actions from "@/actions";
 import { useActionState } from "react";
 
 export default function TopicCreateForm() {
-  const [formState, action] = useActionState(actions.createTopic, 5);
+  const [formState, action] = useActionState(actions.createTopic, {
+    errors: {},
+  });
 
   return (
     <Popover placement="left">
