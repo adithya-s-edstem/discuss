62ba445f7bec8f283efb0a352bc092c4500898aa
Adithya S Sekhar <adithya.s@edstem.com>
2024-11-07 13:17:49 +0530

    98

--- src/actions/create-post.ts
+++ src/actions/create-post.ts
@@ -22,6 +22,7 @@ interface CreatePostFormState {
 }
 
 export async function createPost(
+  slug: string,
   formState: CreatePostFormState,
   formData: FormData
 ): Promise<CreatePostFormState> {
@@ -41,7 +42,19 @@ export async function createPost(
   if (!session || !session.user) {
     return {
       errors: {
-        _form: ["Must be signed in to do this."],
+        _form: ["You must be signed in to do this."],
+      },
+    };
+  }
+
+  const topic = await db.topic.findFirst({
+    where: { slug },
+  });
+
+  if (!topic) {
+    return {
+      errors: {
+        _form: ["Cannot find topic"],
       },
     };
   }
