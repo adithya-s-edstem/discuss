de67474d671bf8bdbd0f337383024df2bcfd5dcc
Adithya S Sekhar <adithya.s@edstem.com>
2024-11-07 16:24:13 +0530

    119

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,10 +4,10 @@ import {
   NavbarBrand,
   NavbarContent,
   NavbarItem,
-  Input,
 } from "@nextui-org/react";
 import React from "react";
 import HeaderAuth from "@/components/header-auth";
+import { SearchInput } from "./search-input";
 
 export default function Header() {
   return (
@@ -19,7 +19,7 @@ export default function Header() {
       </NavbarBrand>
       <NavbarContent justify="center">
         <NavbarItem>
-          <Input />
+          <SearchInput />
         </NavbarItem>
       </NavbarContent>
       <NavbarContent justify="end">
