fd7aa07a3a6fdeffaa6ec2376b37892538216586
Adithya S Sekhar <adithya.s@edstem.com>
2024-11-07 16:43:27 +0530

    124

---
+++ src/app/search/page.tsx
@@ -0,0 +1,15 @@
+import { redirect } from "next/navigation";
+
+interface SearchPageProps {
+  searchParams: Promise<{ term: string }>;
+}
+
+export default async function SearchPage({ searchParams }: SearchPageProps) {
+  const { term } = await searchParams;
+
+  if (!term) {
+    redirect("/");
+  }
+
+  return <div>Search Page for {term}</div>;
+}
