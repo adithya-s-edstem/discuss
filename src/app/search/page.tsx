7ffe02c097f5d8ea260c374a5fb0ff9ec68b9458
Adithya S Sekhar <adithya.s@edstem.com>
2024-11-07 16:48:34 +0530

    125

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,4 +1,6 @@
 import { redirect } from "next/navigation";
+import PostList from "@/components/posts/post-list";
+import { fetchPostsBySearchTerm } from "@/db/queries/posts";
 
 interface SearchPageProps {
   searchParams: Promise<{ term: string }>;
@@ -11,5 +13,9 @@ export default async function SearchPage({ searchParams }: SearchPageProps) {
     redirect("/");
   }
 
-  return <div>Search Page for {term}</div>;
+  return (
+    <div>
+      <PostList fetchData={() => fetchPostsBySearchTerm(term)} />
+    </div>
+  );
 }
